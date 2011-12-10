0f3b91e34286b81b3cf5c225450589f23024092c
Karl <karl.baum@gmail.com>
2011-12-10 09:29:59 -0500

    initial project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aiw::Application.config.secret_token = 'fcc1ebcc96473bd02b340bed65b335a0596675fc3138e95908a6dd6bb6101a15bd28127189be756f4a564f6a6f8c2b2487cb152e26cb5fd29693f1b866583277'
